384230a405d5b745c3657d963a1c88311fc1182a
alizee kraxner <a.kraxner@laposte.net>
2024-06-11 17:47:17 +0200

    modif terminée

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,13 +1,14 @@
 class GardensController < ApplicationController
-  before_action :set_garden, only: [:edit, :next_step, :previous_step]
-# :update dans only si retour a la normal
 
   def create
-    raise
     @garden = Garden.new(garden_params)
     @garden.user = current_user
-    @garden.save
-    redirect_to packagechoice_path(@garden)
+
+    if @garden.save
+      redirect_to garden_path(@garden), notice: 'Garden was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
@@ -33,74 +34,9 @@ class GardensController < ApplicationController
 
   def new
     @garden = Garden.new
-    @garden.current_step = 1
-    render :step1
-  end
-
-  def edit
-    raise
-    case @garden.current_step
-    when 1
-      render :step1
-    when 2
-      raise
-      render :step2
-    when 3
-      render :step3
-    when 4
-      render :step4
-    else
-      redirect_to @garden
-    end
-  end
-
-  def next_step
-    @garden.assign_attributes(garden_params)
-    if params[:next]
-      @garden.current_step += 1 if @garden.valid?
-    elsif params[:back]
-      @garden.current_step -= 1 if @garden.current_step > 1
-    end
-
-    @garden.save(validate: false)
-
-    case @garden.current_step
-    when 1
-      render :step1
-    when 2
-      render :step2
-    when 3
-      render :step3
-    when 4
-      render :step4
-    else
-      redirect_to @garden, notice: 'Garden was successfully created.'
-    end
-  end
-
-  def previous_step
-    @garden.current_step -= 1 if @garden.current_step > 1
-    @garden.save(validate: false)
-
-    case @garden.current_step
-    when 1
-      render :step1
-    when 2
-      render :step2
-    when 3
-      render :step3
-    when 4
-      render :step4
-    else
-      redirect_to @garden, notice: 'Garden was successfully created.'
-    end
   end
 
   private
-
-  def set_garden
-    @garden = Garden.find(params[:id])
-  end
 
   def garden_params
     params.require(:garden).permit(:name, :size, :exposition, :address, :photo)
--- test/controllers/garden_form_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class GardenFormControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
